37b17befdd60f934c708b956c82be76e52a27837
Matheus Garcia <37114863+Krush206@users.noreply.github.com>
2023-08-02 19:46:09 -0300

    First commit

---
+++ Shell.cs
@@ -0,0 +1,69 @@
+﻿namespace System;
+
+public class Shell
+{
+  public static void Main(String[] args)
+  {
+    List<String> argv;
+    String cmd;
+    
+    while(true)
+    {
+      Console.Write("> ");
+      cmd = Console.ReadLine();
+      
+      argv = parse(cmd);
+
+      if(argv.Count == 0)
+        continue;
+
+      shexec(argv);
+    }
+  }
+
+  public static List<String> parse(String s)
+  {
+    String ps = new String("");
+    List<String> argv = new List<String>();
+    int i;
+
+    for(i = 0; i < s.Length; i++)
+      switch(s[i])
+      {
+        case ';':
+          if(argv.Count == 0)
+            break;
+          shexec(argv);
+          argv.Clear();
+          break;
+        case ' ':
+          break;
+        default:
+          for(; i < s.Length && s[i] != ' '; i++)
+            ps += s[i];
+
+          argv.Add(ps);
+          ps = new String("");
+          break;
+      }
+
+    return argv;
+  }
+
+  public static void shexec(List<String> argv)
+  {
+    try
+    {
+      String cmd = argv[0];
+      Diagnostics.Process shproc;
+
+      argv.RemoveAt(0);
+      shproc = Diagnostics.Process.Start(cmd, argv);
+      shproc.WaitForExit();
+    }
+    catch(Exception e)
+    {
+      Console.Write(e.Message);
+    }
+  }
+}
